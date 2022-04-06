72a69f5ac896453f12ae1d291dba0356bde8b054
zieeco <zieecosam@gmail.com>
2022-04-06 07:17:05 +0100

    Add a coulumn to the animals table

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE animals (
     escape_attempts integer,
     neutered boolean,
     weight_kg decimal
-)
-ALTER TABLE animals ADD COLUMN species varchar(100);
+);
 
+ALTER TABLE animals ADD COLUMN species varchar(100);
 
