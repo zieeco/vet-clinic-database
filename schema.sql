a9d32e3f8512a4b81b2dc4325e57f4b9cc86f8d0
zieeco <zieecosam@gmail.com>
2022-04-07 16:57:59 +0100

    Update the columns in the tables

--- schema.sql
+++ schema.sql
@@ -14,20 +14,18 @@ ALTER TABLE animals ADD COLUMN species varchar(100);
 
 -- Creating a table named owners with the following columns:
 CREATE TABLE owners (
-    id INT GENERATED ALWAYS AS IDENTITY,
+    id SERIAL PRIMARY KEY,
     full_name varchar(100) NOT NULL,
-    age integer,
-    PRIMARY KEY(id)
+    age integer
 );
 
 -- Creating a table named species with the following columns:
 CREATE TABLE species (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name varchar(100) NOT NULL,
-    PRIMARY KEY(id)
+    id SERIAL PRIMARY KEY,
+    name varchar(100) NOT NULL
 );
 
 -- Modifying animals table by doing the following 🥶
 ALTER TABLE animals DROP COLUMN species;
-ALTER TABLE animals ADD COLUMN species_id integer;
-ALTER TABLE animals ADD COLUMN owner_id integer;
+ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
