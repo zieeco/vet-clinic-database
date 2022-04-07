10f93449a44df33f911dc95dbcfcbae3de88b5f3
zieeco <zieecosam@gmail.com>
2022-04-07 13:18:05 +0100

    Modify animals table and add two more tables

--- schema.sql
+++ schema.sql
@@ -11,3 +11,23 @@ CREATE TABLE animals (
 );
 
 ALTER TABLE animals ADD COLUMN species varchar(100);
+
+-- Creating a table named owners with the following columns:
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name varchar(100) NOT NULL,
+    age integer,
+    PRIMARY KEY(id)
+);
+
+-- Creating a table named species with the following columns:
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name varchar(100) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+-- Modifying animals table by doing the following 🥶
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id integer;
+ALTER TABLE animals ADD COLUMN owner_id integer;
