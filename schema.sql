9b7884382bcaed1b0784b8253efeca72a90496f1
zieeco <zieecosam@gmail.com>
2022-04-09 08:35:04 +0100

    Add vets table, and 2 other joining tables

--- schema.sql
+++ schema.sql
@@ -29,3 +29,30 @@ CREATE TABLE species (
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+-- Creating a table named vets with the following columns:
+CREATE TABLE vets (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    age INT,
+    date_of_graduation date
+);
+
+-- Joining Table for vets & species table: 🥶
+CREATE TABLE specializations (
+    vets_id INT NOT NULL,
+    species_id INT NOT NULL,
+    FOREIGN KEY (vets_id) REFERENCES vets(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY(vets_id, species_id)
+);
+
+-- Joining Table for animals & vets table: 🥶
+CREATE TABLE visits (
+    animals_id INT NOT NULL,
+    vets_id INT NOT NULL,
+    date_of_visit date,
+    FOREIGN KEY (animals_id) REFERENCES animals(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (vets_id) REFERENCES vets(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    PRIMARY KEY (animals_id, vets_id)
+);
